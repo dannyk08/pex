2f89d54c6e7fc42bd82943a15413a0424b9ddcd0
Daniel Romero <romerodaniel.k90@gmail.com>
2018-12-26 22:09:52 -0800

    initial commit: first test passes

---
+++ formsErrors.js
@@ -0,0 +1,11 @@
+const Immutable = require('immutable')
+
+function transformErrors(formErrors) {
+  return Immutable.Map(Object.keys(formErrors).reduce((prev, errorKey) => {
+    return { ...prev, [errorKey]: formErrors[errorKey].map(e => e + '.').join(' ').trim() }
+  }, {})).toJS()
+}
+
+module.exports = {
+  transformErrors
+}
